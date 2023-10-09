2c72ff7d4841bc9485ca15ead7fbdaca1d23a8e0
ManikandanP24 <147170929+ManikandanP24@users.noreply.github.com>
2023-10-09 21:45:26 +0530

    index.js

--- index.js
+++ index.js
@@ -12,48 +12,22 @@ signInButton.addEventListener('click', ()=>{
 })
 
 
+const signInForm = document.querySelector('.form-container.sign-in-container form');
 
-// Login ID
-    
-   const signInForm = document.querySelector(".sign-in-container form");
+signInForm.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-   
-   signInForm.addEventListener('submit', (event) => {
-        event.preventDefault(); 
+    const username = document.querySelector('.sign-in-container input[type="email"]').value;
+    const password = document.querySelector('.sign-in-container input[type="password"]').value;
 
+    // username and password
+    const validUsername = "admin@gmail.com";
+    const validPassword = "admin";
 
-        const username = document.querySelector('.sign-in-container input[type="email"]').value;
-        const password = document.querySelector('.sign-in-container input[type="password"]').value;
-
-        //  username and password
-
-        const validUsername = "admin@gmail.com";
-        const validPassword = "admin";
-
-        if (username === validUsername && password === validPassword) {
-            
-            alert("Login successful!");
-        } else {
-            
-            alert("Invalid username or password. Please try again.");
-        }
-    });
-
-
-    // sign in page
-
-
-    document.addEventListener('DOMContentLoaded', function () {
-        
-        var signInForm = document.querySelector('.form-container.sign-in-container form');
-
-                signInForm.addEventListener('submit', function (event) {
-            
-            event.preventDefault();
-
-            window.location.href = './voicecon.html';
-        });
-    });
-
-
-
+    if (username === validUsername && password === validPassword) {
+        alert("Login successful!");
+        window.location.href = './voicecon.html';
+    } else {
+        alert("Invalid username or password. Please try again.");
+    }
+});
